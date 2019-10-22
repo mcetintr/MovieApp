bf08ed392aa6ced749c7df0e49917a4d5bad418e
mcetintr <mcetintr@gmail.com>
2019-10-22 22:32:04 +0300

    yeni

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Movie } from '../movie';
 
 @Component({
     selector : 'movies',
-    templateUrl : 'movies.component.html'
+    templateUrl : 'movies.component.html',
+    styles:[
+     `
+     h2 {
+         color : blue;
+     }
+     `
+    ]
 })
 export class MoviesComponent{
-
+    movie : Movie = {
+        id : 1,
+        name : 'Movie Name'
+    }
 }
